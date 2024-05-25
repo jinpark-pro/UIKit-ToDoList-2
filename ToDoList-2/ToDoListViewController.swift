77fab028fd9fd2ed0096e042255cbfcbfdd3838c
Jin Park <jinpark1504@gmail.com>
2024-05-25 11:18:29 +0900

    Add dummy data to Table View

--- ToDoList-2/ToDoListViewController.swift
+++ ToDoList-2/ToDoListViewController.swift
@@ -7,10 +7,13 @@
 
 import UIKit
 
-class ToDoListViewController: UIViewController {
+class ToDoListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     lazy var tableView: UITableView = {
         let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         return tableView
     }()
     
@@ -29,7 +32,26 @@ class ToDoListViewController: UIViewController {
             tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
         ])
     }
-
-
+    // MARK: - UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch (section) {
+        case 0: return "High"
+        case 1: return "Medium"
+        case 2: return "Low"
+        default: return nil
+        }
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
+    }
 }
-
